5b8d3d76b480b3f36e65cfaf311b1d760f4265f4
Vivian Mejia Sanchez <vivian.mejiasanchez@gmail.com>
2021-11-28 20:49:47 +0100

    sidebar with Icons  configurated

--- src/layouts/dashboard/SidebarConfig.jsx
+++ src/layouts/dashboard/SidebarConfig.jsx
@@ -5,7 +5,7 @@ import shoppingBagFill from '@iconify/icons-eva/shopping-bag-fill';
 import fileTextFill from '@iconify/icons-eva/file-text-fill';
 import lockFill from '@iconify/icons-eva/lock-fill';
 import personAddFill from '@iconify/icons-eva/person-add-fill';
-import alertTriangleFill from '@iconify/icons-eva/alert-triangle-fill';
+import BarChartFill from '@iconify/icons-eva/bar-chart-fill';
 
 // ----------------------------------------------------------------------
 
@@ -35,7 +35,7 @@ const sidebarConfig = [
   {
     title: 'advance',
     path: '/dashboard/advance',
-    icon: getIcon(alertTriangleFill)
+    icon: getIcon(BarChartFill)
   },
   {
     title: 'login',
