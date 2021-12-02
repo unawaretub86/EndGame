70bfc157765e4bc0d98f584634bd8da4a82c3984
Vivian Mejia Sanchez <vivian.mejiasanchez@gmail.com>
2021-12-02 18:52:55 +0100

    enrollment list page added

---
+++ src/components/_dashboard/enrollments/index.jsx
@@ -0,0 +1,3 @@
+export { default as ProjectListHead } from './ProjectListHead';
+export { default as ProjectListToolbar } from './ProjectListToolbar.jsx';
+export { default as ProjectMoreMenu } from './projectMoreMenu';
