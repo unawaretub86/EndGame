c6ab448b29c51df63f6e92334957de3270ec1a0c
Andres Roca P <afrokap@gmail.com>
2021-12-08 18:07:46 -0500

    AdmPrj+1.4> Activate prj with conditioned Phase

--- src/components/projects/AdminProject.jsx
+++ src/components/projects/AdminProject.jsx
@@ -1,29 +1,55 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useQuery, useMutation } from '@apollo/client'
-import Button from '@mui/material/Button';
+import { Button, Typography } from '@mui/material';
 
 import { GET_PROJECT_ADMIN_BYID } from '../../graphql/projects/prj-queries';
+import { UPDATE_PROJECT } from '../../graphql/projects/prj-mutations';
 
 export default function AdminProject({ dataID }) {
+    const [stAdminProject, setStAdminProject] = React.useState({});
+    const [mtUpdateProject, { loading: loadMutation }] = useMutation (UPDATE_PROJECT);
+    
     const {data, loading, error} = useQuery(GET_PROJECT_ADMIN_BYID, {
         variables: {
             id: dataID
         }
     })
 
+    useEffect(() => {
+        if (data) {
+            setStAdminProject(data.projectById);
+        }
+    }, [data])
+
     const hdlActivation = () => {
-        console.log('Activation')
+        
+        const nextStatus = stAdminProject.status === 'inactive' ? 'active':'inactive';
+        const toSend = {input: { projectById: dataID , status: nextStatus}};
+        if (stAdminProject.phase === null && nextStatus === 'active'){
+            toSend.input = {...toSend.input, phase: 'started'}
+            mtUpdateProject({variables: toSend});
+            setStAdminProject({...stAdminProject, status: nextStatus, phase: 'started'})
+            return;
+        } 
+        mtUpdateProject({variables: toSend})
+        setStAdminProject({...stAdminProject, status: nextStatus})
     }
+
     if (loading) return <p>Loading...</p>
     if (error) return <p>xx Error xx</p>
 
-    const projectAdminInfo = data.projectById;
-
     return (
-        <>
-            <h2>Administrate: {projectAdminInfo.name}</h2>
+        <>{ stAdminProject.status ?
+            <>
+            <Typography> Project Name: {stAdminProject.name}</Typography>
+            <Typography> Project Leader: {stAdminProject.leader.name.concat(' ',stAdminProject.leader.lastName)}</Typography>
+            <Typography> Project Status: {stAdminProject.status}</Typography>
+            <Typography> Project Phase: {stAdminProject.phase}</Typography>
             <pre>{JSON.stringify(data, null, 2)}</pre>
-            <Button variant="outlined" onClick={hdlActivation}>{ projectAdminInfo === 'inactive' ? 'Activate':'Inactivate'}</Button>         
+            <Button variant="outlined" onClick={hdlActivation}>{ stAdminProject.status === 'inactive' ? 'Activate':'Inactivate'}</Button>         
+            </>
+            : <p>Loading......</p>
+        }
         </>
     )
 }
